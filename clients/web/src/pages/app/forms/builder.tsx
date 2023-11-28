527e1a8005572d8e75c1b6e5a8ee7da6ca2a4453
m3rashid <m3rashid.hussain@gmail.com>
2023-11-28 13:28:30 +0530

    incomplete work

--- clients/web/src/pages/app/forms/builder.tsx
+++ clients/web/src/pages/app/forms/builder.tsx
@@ -1,76 +1,114 @@
-import { Button, Form, Radio } from 'antd';
+import { StyleProvider } from '@ant-design/cssinjs';
+import Editor, { Monaco } from '@monaco-editor/react';
+import { Theme as AntDTheme } from '@rjsf/antd';
+import { withTheme } from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+import { Button, Card } from 'antd';
 import React from 'react';
 
-import FormBuilder from '../../../components/forms/builder';
-import ElementWrapper from '../../../components/forms/elementWrapper';
-import FormJsonEditor from '../../../components/forms/jsonEditor';
-import RightHelperSidebar from '../../../components/forms/rightSidebar';
-import useFormbuilder from '../../../components/forms/useFormbuilder';
 import PageContainer from '../../../components/pageContainer';
 
+const schema: RJSFSchema = {
+  title: 'A registration form',
+  description: 'A simple form example.',
+  type: 'object',
+  required: ['firstName', 'lastName'],
+  properties: {
+    firstName: {
+      type: 'string',
+      title: 'First name',
+      default: 'Chuck',
+    },
+    lastName: {
+      type: 'string',
+      title: 'Last name',
+    },
+    roll: {
+      type: 'string',
+      title: 'Roll',
+      enum: ['admin', 'user', 'guest'],
+    },
+    age: {
+      type: 'integer',
+      title: 'Age',
+    },
+    bio: {
+      type: 'string',
+      title: 'Bio',
+    },
+    password: {
+      type: 'string',
+      title: 'Password',
+      minLength: 3,
+    },
+    telephone: {
+      type: 'string',
+      title: 'Telephone',
+      minLength: 10,
+    },
+  },
+};
+
+const uiSchema = {
+  firstName: {
+    'ui:autofocus': true,
+    'ui:emptyValue': '',
+    'ui:placeholder':
+      'ui:emptyValue causes this field to always be valid despite being required',
+    'ui:autocomplete': 'family-name',
+    'ui:enableMarkdownInDescription': true,
+    'ui:description':
+      'Make text **bold** or *italic*. Take a look at other options [here](https://probablyup.com/markdown-to-jsx/).',
+  },
+  lastName: {
+    'ui:autocomplete': 'given-name',
+    'ui:enableMarkdownInDescription': true,
+    'ui:description':
+      'Make things **bold** or *italic*. Embed snippets of `code`. <small>And this is a small texts.</small> ',
+  },
+  age: {
+    'ui:widget': 'updown',
+    'ui:title': 'Age of person',
+    'ui:description': '(earth year)',
+  },
+  bio: {
+    'ui:widget': 'textarea',
+  },
+  password: {
+    'ui:widget': 'password',
+    'ui:help': 'Hint: Make it strong!',
+  },
+  telephone: {
+    'ui:options': {
+      inputType: 'tel',
+    },
+  },
+};
+
+const Form = withTheme(AntDTheme);
+
 const FormBuilderPage: React.FC = () => {
-  const {
-    elements,
-    setMode,
-    mode,
-    selectedElement,
-    showFormParentProps,
-    setElements,
-  } = useFormbuilder();
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <PageContainer>
-      <div className='flex h-screen'>
-        <div className='w-full overflow-auto'>
-          <div className='bg-white h-[40px] p-2 flex items-center justify-between'>
-            <Radio.Group value={mode} onChange={(e) => setMode(e.target.value)}>
-              <Radio.Button value='edit'>Visual Editor</Radio.Button>
-              <Radio.Button value='json'>JSON Editor</Radio.Button>
-              <Radio.Button value='preview'>Preview</Radio.Button>
-            </Radio.Group>
-
-            <Button disabled={!selectedElement} onClick={showFormParentProps}>
-              Modify Form Container
-            </Button>
-          </div>
-
-          <div className='h-[calc(100vh-88px)] m-2 mb-0 bg-[url(/paper.svg)] '>
-            {mode === 'json' ? (
-              <FormJsonEditor
-                onChange={setElements}
-                initialValue={{ meta: elements }}
-              />
-            ) : elements.length > 0 ? (
-              mode === 'preview' ? (
-                <div className='bg-white max-w-xl mx-auto rounded-sm'>
-                  <Form layout='vertical' style={{ padding: 8 }}>
-                    <FormBuilder meta={elements} />
-                  </Form>
-                </div>
-              ) : (
-                <div className='bg-white max-w-xl mx-auto rounded-sm'>
-                  <Form layout='vertical' style={{ padding: 8 }}>
-                    <FormBuilder
-                      meta={elements.map((el) => ({
-                        ...el,
-                        render: (WidgetField: any) => (props: any) => {
-                          return (
-                            <ElementWrapper id={el.key} key={el.key}>
-                              <WidgetField {...props} />
-                            </ElementWrapper>
-                          );
-                        },
-                      }))}
-                    />
-                  </Form>
-                </div>
-              )
-            ) : null}
-          </div>
-        </div>
-
-        {mode !== 'json' ? <RightHelperSidebar /> : null}
-      </div>
+      <Editor height={600} language='json' />
+      <StyleProvider>
+        <Card className='w-80'>
+          <Form
+            schema={schema}
+            uiSchema={uiSchema}
+            validator={validator}
+            onSubmit={onSubmit}
+          >
+            <div>
+              <Button type='primary'>Submit</Button>
+              <Button>Cancel</Button>
+            </div>
+          </Form>
+        </Card>
+      </StyleProvider>
     </PageContainer>
   );
 };
